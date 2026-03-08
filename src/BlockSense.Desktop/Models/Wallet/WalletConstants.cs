21f4208626f70d2def07a30ed6ebba1fea22cf76
durismaros <durismaros01@gmail.com>
2026-03-08 18:32:06 +0100

    - Added: WalletConstants.cs

---
+++ src/BlockSense.Desktop/Models/Wallet/WalletConstants.cs
@@ -0,0 +1,33 @@
+﻿using NBitcoin;
+
+namespace BlockSense.Desktop.Models.Wallet
+{
+    public static class BitcoinFees
+    {
+        public const decimal Default = 0.0001m;
+    }
+
+    public static class EthereumFees
+    {
+        public const long DefaultGasLimit = 21_000;
+
+        public const decimal DefaultGasPriceGwei = 20m;
+    }
+
+    public static class BitcoinChain
+    {
+        public static readonly Network CurrentNetwork = Network.TestNet;
+
+        // Testnet = "m/44'/1'/0'/0"
+        public static readonly KeyPath DerivationPath = new("m/44'/0'/0'/0");
+    }
+
+    public static class EthereumChain
+    {
+        // Sepolia = 11155111
+        // Mainnet = 1
+        public const int CurrentNetwork = 11155111;
+
+        public static readonly KeyPath DerivationPath = new("m/44'/60'/0'/0");
+    }
+}
