8342688f161e199ce459e52af27c74858e4dd2c9
durismaros <durismaros01@gmail.com>
2026-03-12 20:14:43 +0100

    - Updated: HttpClient URL

--- src/BlockSense.Desktop/App.axaml.cs
+++ src/BlockSense.Desktop/App.axaml.cs
@@ -56,7 +56,7 @@ namespace BlockSense.Desktop
 
             services.AddHttpClient<IApiClient, ApiClient>(client =>
             {
-                client.BaseAddress = new Uri("https://localhost:7262");
+                client.BaseAddress = new Uri("https://unicorn-casual-yeti.ngrok-free.app");
                 client.Timeout = TimeSpan.FromSeconds(30);
                 client.DefaultRequestHeaders.Accept.Add(
                     new MediaTypeWithQualityHeaderValue("application/json"));
